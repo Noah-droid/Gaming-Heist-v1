cd495b06461c631c330598fa9ffab7756216d6fd
Noah <76827471+Noah-droid@users.noreply.github.com>
2022-12-25 18:19:49 +0100

    Update forms.py

--- base/forms.py
+++ base/forms.py
@@ -6,17 +6,17 @@ from django import forms
 class UserForm(ModelForm):
     class Meta:
         model = User 
-        fields = ['username', 'name', 'email', 'avatar' ,'bio', 'twitter', 'linkedin', 'facebook', 'github', 'website']    
+        fields = ['username', 'name', 'email', 'avatar' ,'bio', 'cod_id', 'pubg_id']    
         widgets = {
             'username': forms.TextInput(attrs={'class':'form-field--input'}),
             'name':forms.TextInput(attrs={'class':'form-field--input'}),
             'email':forms.EmailInput(attrs={'class':'form-field--input'}),
             'bio':forms.Textarea(attrs={'class':'form-field--input-txarea'}),
-            'twitter': forms.TextInput(attrs={'class':'form-field--input'}),
-            'linkedin':forms.TextInput(attrs={'class':'form-field--input'}),
-            'facebook':forms.TextInput(attrs={'class':'form-field--input'}),
-            'github':forms.TextInput(attrs={'class':'form-field--input'}),
-            'website':forms.TextInput(attrs={'class':'form-field--input'})
+            # 'twitter': forms.TextInput(attrs={'class':'form-field--input'}),
+            # 'linkedin':forms.TextInput(attrs={'class':'form-field--input'}),
+            # 'facebook':forms.TextInput(attrs={'class':'form-field--input'}),
+            # 'github':forms.TextInput(attrs={'class':'form-field--input'}),
+            # 'website':forms.TextInput(attrs={'class':'form-field--input'})
         }
 class SubmissionForm(ModelForm):
     class Meta:
@@ -26,9 +26,11 @@ class SubmissionForm(ModelForm):
 class CustomUserCreateForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ['username', 'email', 'name', 'password1', 'password2']
+        fields = ['username', 'email','cod_id', 'pubg_id', 'name', 'password1', 'password2']
         widgets = {
             'username': forms.TextInput(attrs={'class':'form-field--input'}),
             'name':forms.TextInput(attrs={'class':'form-field--input'}),
-            'email':forms.EmailInput(attrs={'class':'form-field--input'})
+            'email':forms.EmailInput(attrs={'class':'form-field--input'}),
+            'cod_id':forms.TextInput(attrs={'class':'form-field--input'}),
+            'pubg_id':forms.TextInput(attrs={'class':'form-field--input'})
         }
